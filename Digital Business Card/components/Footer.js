d60e3133f7817cd241c6b7c27598ce7eb25ac06c
mmustafa93 <sheikh.mustafa93@gmail.com>
2022-09-19 04:12:18 +0500

    first commit

---
+++ Digital Business Card/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <small id="icons--socials">
+            <img src="../images/TwitterIcon.png" className="icon--twitter" />
+            <img src="../images/FacebookIcon.png" className="icon--facebook" />
+            <img src="../images/InstagramIcon.png" className="icon--instagram" />
+            <img src="../images/GitHubIcon.png" className="icon--github" />
+        </small>
+    )
+}
---
+++ Digital Business Card/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <div>
+            <h2 className="interests--title">Interests</h2>
+            <p className="interests--info">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    )
+}
