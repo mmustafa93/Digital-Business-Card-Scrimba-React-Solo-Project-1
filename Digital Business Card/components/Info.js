6075d46a0a11850b04f60d66014789342a6ad0e8
mmustafa93 <sheikh.mustafa93@gmail.com>
2022-09-19 04:17:44 +0500

    updated the name of one of the images

--- Digital Business Card/components/Info.js
+++ Digital Business Card/components/Info.js
@@ -1,17 +1,17 @@
-import React from "react"
+import React from "react";
 
 export default function Info() {
-    return (
-        <header>
-            <img src="../images/123.jpg" className="info--img" />
-            <h1 className="info--title">Muhammad Mustafa</h1>
-            <h4 className="info--role">Frontend Developer</h4>
-            <p className="info--website">mmustafa.website</p>
-            
-            <div className="img--icons">
-                <img src="../images/Email.png" />
-                <img src="../images/LinkedIn.png" />
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <img src="../images/display-img.jpg" className="info--img" />
+      <h1 className="info--title">Muhammad Mustafa</h1>
+      <h4 className="info--role">Frontend Developer</h4>
+      <p className="info--website">mmustafa.website</p>
+
+      <div className="img--icons">
+        <img src="../images/Email.png" />
+        <img src="../images/LinkedIn.png" />
+      </div>
+    </header>
+  );
 }
